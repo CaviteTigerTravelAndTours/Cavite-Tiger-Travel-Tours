fcf9dbfee9650cd2ec454a609f65acabdf9c6b5c
Cavite Tiger Travel And Tours <robertohormillosatababajr@gmail.com>
2024-11-25 16:59:02 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    // Your email address
+    $to = "cavitetigers2021@gmail.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "There was an error sending your message. Please try again later.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
